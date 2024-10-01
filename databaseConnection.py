736271c2e628a76c32c59ad4ef64b3bee0515edc
shikhar <shikhardaftari23@gmail.com>
2024-09-30 19:57:12 -0700

    sample DB connection

--- databaseConnection.py
+++ databaseConnection.py
@@ -1,4 +1,15 @@
-#delete after use
-price = 10
-finalprice = ((price*70)/100) + (price*5)/100 + 7.5
-print(finalprice)
+#This sample file demonstrate SQL capabilities with python
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="shikhar",
+  database="classicmodels"
+)
+
+mycursor = mydb.cursor()
+mycursor.execute("SELECT * FROM offices")
+offices = mycursor.fetchall()
+for x in offices:
+  print(x)
